f93a2fb1aaad07137e31f498e01327d9bf0622ad
Shangfeng Liu <codershangfeng@gmail.com>
2021-04-08 16:50:02 +0800

    Add request parameters and log.

--- src/main/kotlin/me/codershangfeng/demo/springkotlin/controller/GreetingController.kt
+++ src/main/kotlin/me/codershangfeng/demo/springkotlin/controller/GreetingController.kt
@@ -1,12 +1,21 @@
 package me.codershangfeng.demo.springkotlin.controller
 
 import me.codershangfeng.demo.springkotlin.model.Greeting
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GreetingController {
 
+    companion object {
+        private val log = LoggerFactory.getLogger(Greeting::class.java)
+    }
+
     @GetMapping("/greeting")
-    fun greeting(): Greeting = Greeting("1", "Hello, world")
+    fun greeting(@RequestParam id: String?): Greeting {
+        log.info("Received greeting request from id={$id}")
+        return Greeting(id ?: "1", "Hello, world")
+    }
 }
